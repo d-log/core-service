fdd46325cea45c22598200ab121122b0ea48c5c4
Marcus Chiu <marcus.chiu@marcuss-mbp.lan>
2018-03-30 03:44:08 -0500

    removed eureka

--- src/main/java/com/loggerproject/coreservice/CoreServiceApplication.java
+++ src/main/java/com/loggerproject/coreservice/CoreServiceApplication.java
@@ -3,19 +3,12 @@ package com.loggerproject.coreservice;
 import com.loggerproject.microserviceglobalresource.server.annotation.mongo.MongoDBConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * @SpringBootApplication -
- * @EnableEurekaClient -
- * @MongoDBConfig -
- */
 @SpringBootApplication
-@EnableEurekaClient
 @MongoDBConfig
 public class CoreServiceApplication {
 
