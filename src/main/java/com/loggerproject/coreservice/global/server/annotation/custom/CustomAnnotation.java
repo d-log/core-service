b7902919e3a6a229bcfa6a8761a50308c4819434
marcus.chiu <marcuschiu9@gmail.com>
2018-04-07 17:59:15 -0500

    clean up

--- src/main/java/com/loggerproject/coreservice/global/server/annotation/custom/CustomAnnotation.java
+++
@@ -1,4 +0,0 @@
-package com.loggerproject.coreservice.global.server.annotation.custom;
-
-public class CustomAnnotation {
-}
--- src/main/java/com/loggerproject/coreservice/server/ImageInternalLogData.java
+++
@@ -1,9 +0,0 @@
-package com.loggerproject.coreservice.server;
-
-import lombok.Data;
-
-@Data
-public class ImageInternalLogData extends ALogData {
-    String imageURL;
-    String imageID;
-}
