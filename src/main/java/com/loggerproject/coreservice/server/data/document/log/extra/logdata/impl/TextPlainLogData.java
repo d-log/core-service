8c982646e35f3535164bc10c5c387f9f69c4b2cd
marcus.chiu <marcuschiu9@gmail.com>
2018-04-09 23:33:45 -0500

    text to plain text

--- src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/TextPlainLogData.java
+++ src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/TextPlainLogData.java
@@ -1,13 +1,11 @@
 package com.loggerproject.coreservice.server.data.document.log.extra.logdata.impl;
 
-import com.loggerproject.coreservice.server.data.document.log.extra.logdata.impl.pojo.text.TextType;
 import lombok.Data;
 
 @Data
-public class TextLogData {
+public class TextPlainLogData {
 
     // required
-    TextType textType;
     String text;
 
     // optional
--- src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/TextPlainLogDataScrubberValidator.java
+++ src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/TextPlainLogDataScrubberValidator.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 @Service
-public class TextLogDataScrubberValidator extends ALogDataScrubberValidator<TextLogData> {
+public class TextPlainLogDataScrubberValidator extends ALogDataScrubberValidator<TextPlainLogData> {
     @Override
-    public TextLogData scrubAndValidateLogData(TextLogData data) {
-        Assert.notNull(data.getTextType(), "TextLogData.textType cannot be empty");
+    public TextPlainLogData scrubAndValidateLogData(TextPlainLogData data) {
+        Assert.notNull(data.getText(), "TextPlainLogData.text cannot be empty");
         return data;
     }
 }
