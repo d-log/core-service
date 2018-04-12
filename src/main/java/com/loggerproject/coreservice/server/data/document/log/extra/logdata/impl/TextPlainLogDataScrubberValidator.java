90da389cde6431c2ad201537fdc175996235ea01
marcus.chiu <marcuschiu9@gmail.com>
2018-04-11 23:53:08 -0500

    LogDataValidator - added asserts

--- src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/TextPlainLogDataScrubberValidator.java
+++ src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/TextPlainLogDataScrubberValidator.java
@@ -3,12 +3,13 @@ package com.loggerproject.coreservice.server.data.document.log.extra.logdata.imp
 import com.loggerproject.coreservice.server.data.document.log.extra.logdata.ALogDataScrubberValidator;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import org.apache.commons.lang3.StringUtils;
 
 @Service
 public class TextPlainLogDataScrubberValidator extends ALogDataScrubberValidator<TextPlainLogData> {
     @Override
     public TextPlainLogData scrubAndValidateLogData(TextPlainLogData data) {
-        Assert.notNull(data.getText(), "TextPlainLogData.text cannot be empty");
+        Assert.isTrue(StringUtils.isNotEmpty(data.getText()), "TextPlainLogData.text cannot be empty");
         return data;
     }
 }
--- src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/VideoYouTubeLogDataScrubberValidator.java
+++ src/main/java/com/loggerproject/coreservice/server/data/document/log/extra/logdata/impl/VideoYouTubeLogDataScrubberValidator.java
@@ -1,12 +1,15 @@
 package com.loggerproject.coreservice.server.data.document.log.extra.logdata.impl;
 
 import com.loggerproject.coreservice.server.data.document.log.extra.logdata.ALogDataScrubberValidator;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class VideoYouTubeLogDataScrubberValidator extends ALogDataScrubberValidator<VideoYouTubeLogData> {
     @Override
     public VideoYouTubeLogData scrubAndValidateLogData(VideoYouTubeLogData data) {
+        Assert.isTrue(StringUtils.isNotEmpty(data.getVideoID()), "VideoYouTubeLogData data.videoID cannot be empty");
         return data;
     }
 }
