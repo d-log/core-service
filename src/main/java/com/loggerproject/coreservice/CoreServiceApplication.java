2da92d5476061b4750799affad4f62caba3df514
marcus.chiu <marcuschiu9@gmail.com>
2018-05-06 19:09:16 -0500

    CORS - changed

--- src/main/java/com/loggerproject/coreservice/CoreServiceApplication.java
+++ src/main/java/com/loggerproject/coreservice/CoreServiceApplication.java
@@ -34,15 +34,15 @@ public class CoreServiceApplication {
                                 // local machine
                                 "http://localhost:4200",
                                 "http://localhost:8080",
+                                "http://192.168.86.250:8888",
                                 // PUBLIC ADDRESSES - maybe private
                                 // global - default ports
                                 "http://life.marcuschiu.com:8080",
                                 "http://life.marcuschiu.com:4200",
                                 "http://life.marcuschiu.com:80",
                                 "http://life.marcuschiu.com",
-                                "http://72.180.103.50:8888")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("http://192.168.86.250:8888");
+                                "http://72.180.103.50:8888");
+//                        .allowedOrigins("http://192.168.86.250:8888");
             }
         };
     }
