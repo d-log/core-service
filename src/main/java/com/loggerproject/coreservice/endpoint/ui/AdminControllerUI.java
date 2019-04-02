5b6d84f220bf1e181db5bc19ad708bf7140b7681
Marcus Chiu <marcuschiu9@gmail.com>
2019-04-02 01:09:00 -0500

    cross origin to ui endpoint

--- src/main/java/com/loggerproject/coreservice/endpoint/ui/AdminControllerUI.java
+++ src/main/java/com/loggerproject/coreservice/endpoint/ui/AdminControllerUI.java
@@ -8,20 +8,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class AdminControllerUI {
 
-    @CrossOrigin(origins = "http://192.168.86.218")
-    @GetMapping("/")
-    public @ResponseBody
-    String test() {
-        return "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "  <form method=\"POST\" action=\"/api/image-upload/file\" enctype=\"multipart/form-data\">\n" +
-                "    <input type=\"file\" name=\"file\" /><br/><br/>\n" +
-                "    <input type=\"submit\" value=\"Submit\" />\n" +
-                "  </form>\n" +
-                "</body>\n" +
-                "</html>";
-    }
+//    @GetMapping("/")
+//    public @ResponseBody
+//    String test() {
+//        return "<!DOCTYPE html>\n" +
+//                "<html>\n" +
+//                "<head>\n" +
+//                "</head>\n" +
+//                "<body>\n" +
+//                "  <form method=\"POST\" action=\"/api/image-upload/file\" enctype=\"multipart/form-data\">\n" +
+//                "    <input type=\"file\" name=\"file\" /><br/><br/>\n" +
+//                "    <input type=\"submit\" value=\"Submit\" />\n" +
+//                "  </form>\n" +
+//                "</body>\n" +
+//                "</html>";
+//    }
 }
