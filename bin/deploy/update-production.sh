23173ac5d6a8940f595ce2dfd945d7f07e10ce7b
Marcus Chiu <marcuschiu9@gmail.com>
2020-06-30 00:44:51 -0500

    added CORS for https://doc.marcuschiu.com

--- bin/deploy/update-production.sh
+++ bin/deploy/update-production.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-ssh pi@192.168.86.28 << EOF
+ssh ubuntu-server << EOF
     cd core-service/core-service
     git pull origin master
     ./bin/deploy/kill_core-service.sh
-    nohup mvn spring-boot:run &
-    tail -f nohup.out
+# systemd redeploys
+#    nohup mvn spring-boot:run &
+#    tail -f nohup.out
 EOF
